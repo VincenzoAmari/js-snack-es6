1c6cb6b443ea2e1ac3d0c74ec87c70d12b698fe1
VincenzoAmari <vincenzo.am99@gmail.com>
2025-01-22 15:54:27 +0100

    const biciclette

---
+++ Snack1/main.js
@@ -0,0 +1,12 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const biciclette = [
+  { nome: "Bianchi Oltre XR4", peso: 6.8 },
+  { nome: "Cannondale SuperSix EVO", peso: 7.2 },
+  { nome: "Specialized S-Works Tarmac", peso: 6.7 },
+  { nome: "Trek Émonda SLR", peso: 6.9 },
+  { nome: "Giant TCR Advanced SL", peso: 7.0 },
+];
